578dd983fd409270f03e2739a312385a0843b4fa
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-25 06:57:38 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -3,7 +3,6 @@ package;
 import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxGame;
-import haxe.io.Path;
 import flixel.FlxState;
 import flixel.addons.transition.FlxTransitionableState;
 import openfl.Assets;
@@ -12,6 +11,16 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.system.System;
+#if cpp
+import cpp.vm.Gc;
+#elseif hl
+import hl.Gc;
+#elseif java
+import java.vm.Gc;
+#elseif neko
+import neko.vm.Gc;
+#end
+
 #if android
 import android.content.Context;
 import android.os.Build;
@@ -37,7 +46,7 @@ class Main extends Sprite {
 
 	public function new() {
 		super();
-		
+
 		if (stage != null) {
 			init();
 		}
@@ -68,6 +77,8 @@ class Main extends Sprite {
 		Storage.copyNecessaryFiles();
 		#end
 		
+		Lib.application.window.onClose.add(PlayState.onWinClose);
+
 		#if !debug
 		initialState = TitleState;
 		#end
@@ -86,9 +97,11 @@ class Main extends Sprite {
 				Paths.clearStoredMemory(true);
 				FlxG.bitmap.dumpCache();
 			}
+			clearMajor();
 		});
 		FlxG.signals.postStateSwitch.add(function () {
 			Paths.clearUnusedMemory();
+			clearMajor();
 			Main.skipNextDump = false;
 		});
 
@@ -98,26 +111,43 @@ class Main extends Sprite {
 		FlxG.autoPause = false;
 		#end
 
-		FlxG.signals.gameResized.add(function (w, h) {
-			//if(fpsVar != null)
-				//fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
-		     if (FlxG.cameras != null) {
-			   for (cam in FlxG.cameras.list) {
-				   @:privateAccess
-				   if (cam != null && cam._filters != null)
-					resetSpriteCache(cam.flashSprite);
-			   }
-			}
-
-			if (FlxG.game != null)
-			resetSpriteCache(FlxG.game);
-		});
+		FlxG.signals.gameResized.add(onResizeGame);
 	}
 
-	static function resetSpriteCache(sprite:Sprite):Void {
+	function onResizeGame(w:Int, h:Int) {
+		fixShaderSize(this);
+		if (FlxG.game != null) fixShaderSize(FlxG.game);
+
+		if (FlxG.cameras == null) return;
+		for (cam in FlxG.cameras.list) {
+			@:privateAccess
+			if (cam != null && (cam._filters != null || cam._filters != []))
+				fixShaderSize(cam.flashSprite);
+		}	
+	}
+
+	function fixShaderSize(sprite:Sprite) // Shout out to Ne_Eo for bringing this to my attention
+	{
 		@:privateAccess {
-		        sprite.__cacheBitmap = null;
-			sprite.__cacheBitmapData = null;
+			if (sprite != null)
+			{
+				sprite.__cacheBitmap = null;
+				sprite.__cacheBitmapData = null;
+				sprite.__cacheBitmapData2 = null;
+				sprite.__cacheBitmapData3 = null;
+				sprite.__cacheBitmapColorTransform = null;
+			}
 		}
+	}
+
+	public static function clearMajor() {
+		#if cpp
+		Gc.run(true);
+		Gc.compact();
+		#elseif hl
+		Gc.major();
+		#elseif (java || neko)
+		Gc.run(true);
+		#end
 	}
 }
